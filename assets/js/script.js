aa17e6c9a9cc46a255fa8b9e55aabceb629cac42
HelloTalib <abutalib4599@gmail.com>
2023-12-17 20:36:44 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // do something
+
+    console.log('DOM fully loaded and parsed');
+    window.addEventListener('load', function () {
+        console.log(gsap);
+
+        // do something
+        // console.log('All assets are loaded');
+
+
+    }
+    )
+}
+);
+
+// Path: assets/js/script.js
